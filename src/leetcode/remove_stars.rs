f8a8ed62e08989957382a8e87f6ae31b852ab9db
DraconDev <dracsharp@gmail.com>
2023-11-30 13:07:12 +0000

    add remove_stars

---
+++ src/leetcode/remove_stars.rs
@@ -0,0 +1,11 @@
+pub fn remove_stars(s: String) -> String {
+    let mut result = String::new();
+    for c in s.chars() {
+        if c == '*' {
+            result.pop();
+        } else {
+            result.push(c);
+        }
+    }
+    result
+}
