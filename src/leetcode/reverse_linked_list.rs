a95773dbb011ed1e7e6914efaf0ce89ce640d6d7
DraconDev <dracsharp@gmail.com>
2023-12-08 14:57:45 +0000

    add pair_sum

--- src/leetcode/reverse_linked_list.rs
+++ src/leetcode/reverse_linked_list.rs
@@ -12,3 +12,25 @@ pub fn reverse_linked_list(mut head: Option<Box<ListNode>>) -> Option<Box<ListNo
     }
     prev
 }
+
+pub fn pair_sum(mut head: Option<Box<ListNode>>) -> i32 {
+    let mut head2 = head;
+    let mut length = 0;
+    let mut elements = vec![];
+    while let Some(mut node) = head2 {
+        length += 1;
+        elements.push(node.val);
+        head2 = node.next.take();
+    }
+
+    let mut highest = 0;
+    let mut i = 0;
+    while i < length / 2 {
+        let temp_sum = elements[i] + elements[length - 1 - i];
+        if temp_sum > highest {
+            highest = temp_sum
+        }
+        i += 1
+    }
+    highest
+}
