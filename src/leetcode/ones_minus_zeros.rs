3e7de21122b29b56192550a77df58b6820753c05
DraconDev <dracsharp@gmail.com>
2023-12-14 13:58:34 +0000

    add ones_minus_zeros

--- src/leetcode/ones_minus_zeros.rs
+++ src/leetcode/ones_minus_zeros.rs
@@ -7,9 +7,11 @@ pub fn ones_minus_zeros(grid: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
             match grid[i][j] {
                 1 => {
                     row[i] += 1;
+                    col[j] += 1;
                 }
                 0 => {
-                    col[j] += 1;
+                    col[j] -= 1;
+                    row[i] -= 1;
                 }
                 _ => {}
             }
@@ -19,17 +21,7 @@ pub fn ones_minus_zeros(grid: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
     let mut result: Vec<Vec<i32>> = vec![vec![0; grid[0].len()]; grid.len()];
     for i in 0..grid.len() {
         for j in 0..grid[0].len() {
-            match grid[i][j] {
-                1 => {
-                    let countRow = row[i] - grid.len() as i32 - row[i];
-                    let countCol = col[j] - grid[0].len() as i32 - col[j];
-                    result[i][j] = 1 + countRow + countCol;
-                }
-                0 => {
-                    result[i][j] = -1 + row[i] - col[j] + grid.len() as i32 - col[j];
-                }
-                _ => {}
-            }
+            result[i][j] = row[i] + col[j];
         }
     }
     result
