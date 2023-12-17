eeb0957195633bf8ebcf241a1af519238a28ec02
DraconDev <dracsharp@gmail.com>
2023-12-17 14:20:37 +0000

    upd food ratings performance

--- src/leetcode/food_ratings.rs
+++ src/leetcode/food_ratings.rs
@@ -4,21 +4,51 @@ struct FoodInfo {
     cuisine: String,
     rating: i32,
 }
+
+struct CuisineInfo {
+    highest_rated: String,
+    highest_rating: i32,
+}
 pub struct FoodRatings {
     food_map: HashMap<String, FoodInfo>,
+    cuisine_map: HashMap<String, CuisineInfo>,
 }
 
 impl FoodRatings {
     pub fn new(foods: Vec<String>, cuisines: Vec<String>, ratings: Vec<i32>) -> Self {
-        let mut food_map = HashMap::new();
+        let mut food_map: HashMap<String, FoodInfo> = HashMap::new();
+        let mut cuisine_map: HashMap<String, CuisineInfo> = HashMap::new();
         for (i, food) in foods.into_iter().enumerate() {
+            if !cuisine_map.contains_key(&cuisines[i]) {
+                cuisine_map.insert(
+                    cuisines[i].clone(),
+                    CuisineInfo {
+                        highest_rated: food.clone(),
+                        highest_rating: ratings[i].clone(),
+                    },
+                );
+            } else {
+                if ratings[i] > cuisine_map.get(&cuisines[i]).unwrap().highest_rating {
+                    cuisine_map.get_mut(&cuisines[i]).unwrap().highest_rated = food.clone();
+                    cuisine_map.get_mut(&cuisines[i]).unwrap().highest_rating = ratings[i];
+                } else if ratings[i] == cuisine_map.get(&cuisines[i]).unwrap().highest_rating
+                    && food < cuisine_map.get(&cuisines[i]).unwrap().highest_rated
+                {
+                    cuisine_map.get_mut(&cuisines[i]).unwrap().highest_rated = food.clone();
+                }
+            }
+
             let info = FoodInfo {
                 cuisine: cuisines[i].clone(),
                 rating: ratings[i],
             };
-            food_map.insert(food, info);
+
+            food_map.insert(food.clone(), info);
         }
-        FoodRatings { food_map }
+        FoodRatings {
+            food_map,
+            cuisine_map,
+        }
     }
 
     pub fn change_rating(&mut self, food: String, new_rating: i32) {
@@ -26,22 +56,10 @@ impl FoodRatings {
     }
 
     pub fn highest_rated(&self, cuisine: String) -> String {
-        let mut max_rating = 0;
-        let mut max_food: Option<&String> = None;
-        for (food, info) in self.food_map.iter() {
-            if info.cuisine == cuisine {
-                if info.rating > max_rating {
-                    max_rating = info.rating;
-                    max_food = Some(food);
-                } else if info.rating == max_rating {
-                    if let Some(max) = max_food {
-                        if food < max {
-                            max_food = Some(food);
-                        }
-                    }
-                }
-            }
-        }
-        max_food.map_or(String::new(), |f| f.clone())
+        self.cuisine_map
+            .get(&cuisine)
+            .unwrap()
+            .highest_rated
+            .clone()
     }
 }
